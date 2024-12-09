9b4b884c884a925f48fd19db89200c8865eae6f1
yinchenhsieh <yin.c.hsieh@gmail.com>
2024-12-09 11:37:00 -0900

    allow for batch and single extract for prot and nucl

--- get_ncbi_seqs.py
+++ get_ncbi_seqs.py
@@ -13,6 +13,8 @@ import argparse
 
 Entrez.email = "yhsieh@alaska.edu"
 
+## COMMON FUNCTIONS
+
 def readaccids(infile):
     """Reads in file with accession IDs, returns a list"""
     accids = []
@@ -21,15 +23,66 @@ def readaccids(infile):
             accids.append(line.strip())
     return accids
 
-def batch_get(listofaccids):
-    """Submits Entrez batch request, returns handle"""
-    request = Entrez.epost("protein", id=",".join(listofaccids))
+def check_moltype(somestr):
+    if somestr.lower() in 'proteinaaminoacid':
+        return 'protein'
+    elif somestr.lower() in 'nucleotidednarna':
+        return 'nucleotide'
+
+def batch_get(listofaccids, moltype='protein'):
+    """Submits Entrez batch request for prot (default)
+    or nucl seqs, returns handle.
+    Use when you have relatively high confidence 
+    your accids have existing entries
+    
+    :param listofaccids: list of str
+    :param moltype: str
+    
+    :returns: http.client.HTTPResponse"""
+    #TODO: add error handling
+    nonemptylist = [item for item in listofaccids if item]
+    moltype = check_moltype(moltype)
+    request = Entrez.epost(moltype, id=",".join(nonemptylist))
     result = Entrez.read(request)
     webEnv = result["WebEnv"]
     queryKey = result["QueryKey"]
-    handle = Entrez.efetch(db="protein", retmode="xml", webenv=webEnv, query_key=queryKey)
+    handle = Entrez.efetch(db=moltype,\
+                           retmode="xml",\
+                               webenv=webEnv,\
+                                   query_key=queryKey)
     return handle
 
+def single_get(accid, moltype='protein'):
+    """Submits single Entrez request for prot(default)
+    or nucl seqs, returns handle
+    
+    :param accid: str
+    :param moltype: str
+    
+    :returns: http.client.HTTPResponse"""
+    moltype = check_moltype(moltype)
+    try:
+        handle = Entrez.efetch(db=moltype, id=accid, retmode="xml")
+    except Exception as e:
+        print(e)
+        return 1
+    else:
+        return handle
+
+def parse_entrez_handle_to_list(handleobj):
+    """Parses Entrez handle containing one or more records
+    into dict object
+    
+    :param handleobj: http.client.HTTPResponse
+    
+    :returns: list"""
+    try:
+        recordslist = list(Entrez.parse(handleobj))
+    except Exception as e:
+        print(e)
+        return 1
+    else:
+        return recordslist
 
 def find_organism(entrez_record):
     """Takes in record object, finds organism name"""
@@ -41,6 +94,8 @@ def find_organism(entrez_record):
             for item in featquals:
                 if item['GBQualifier_name'] == 'organism':
                     return item['GBQualifier_value']
+                
+## PROT FUNCTIONS
 
 def find_protein(entrez_record):
     """Takes in record object, finds protein name"""
@@ -52,16 +107,77 @@ def find_protein(entrez_record):
             for item in featquals:
                 if item['GBQualifier_name'] == 'product':
                     return item['GBQualifier_value']
+                
+def find_gene_accid(entrez_record):
+    """Takes in record object, returns GenBank accession"""
+    tag1 = 'GBSeq_feature-table'
+    tag2 = 'GBFeature_quals'
+    tag3 = 'GBQualifier_name'
+    tag4 = 'GBQualifier_value'
+    try:
+        infolist = entrez_record[tag1][-1][tag2]
+    except Exception:
+        return 1
+    else:
+        for gbdict in infolist:
+            if gbdict[tag3] == 'coded_by':
+                accid = gbdict[tag4].split(':')[0]
+                if '(' in accid:
+                    return accid.split('(')[1]
+                else:
+                    return accid
+    
+def get_gene_accid(handleobj):
+    """Takes in handle, iterates find gene accid, returns list"""
+    records = Entrez.parse(handleobj)
+    geneaccid_list = []
+    for record in records:
+        geneaccid_list.append(find_gene_accid(record)) # could append 0/None
+    return geneaccid_list
 
+def write_gene_accid_to_file(accidlist, outfile="accids.txt"):
+    """Writes out accids to a file"""
+    with open(outfile, 'w+') as outf:
+        for item in accidlist:
+            if item: # skip None
+                outf.write(f'{item}\n')
 
-def extract_fasta_info(entrez_record):
+## FASTA FUNCTIONS
+
+def extract_fasta_info(entrez_record, moltype='protein'):
     """Takes in a record object from Entrez handle,
     returns a dict with header and fasta"""
-    accid = entrez_record['GBSeq_primary-accession']
+    moltype=check_moltype(moltype)
     organism = find_organism(entrez_record)
-    protein = find_protein(entrez_record)
     seq = entrez_record['GBSeq_sequence']
-    header = f'{accid}|{protein}|{organism}'
+    if moltype=='protein':
+        accid = entrez_record['GBSeq_primary-accession']
+        protein = find_protein(entrez_record)
+        header = f'{accid}|{protein}|{organism}'
+    elif moltype=='nucleotide':
+        accid = entrez_record['GBSeq_locus']
+        primeaccid = entrez_record['GBSeq_primary-accession']
+        seqdef = entrez_record['GBSeq_definition']
+        header = f'{accid}|{primeaccid}|{seqdef}'
+        
+        #### TODO: extract protid and append to header if it exists
+        # for featitem in entrez_record['GBSeq_feature-table']:
+        #     if not 'GBFeature_quals' in featitem.keys():
+        #         protid = ''
+        #     else:
+        #         ...
+        
+        # if entrez_record['GBSeq_feature-table'][-1]['GBFeature_quals']:
+        #     for entry in entrez_record['GBSeq_feature-table'][-1]['GBFeature_quals']:
+        #         if entry['GBQualifier_name'] == 'protein_id':
+        #             protid = entry['GBQualifier_value']
+        #         else:
+        #             protid = ''
+        # else:
+        #     protid = ''
+        # header = f'{accid}|{primeaccid}|{protid}|{seqdef}'
+        ####
+        
     return {header:seq}
 
 def write_fasta(fasta_dict, out_path,flag='w'):
@@ -73,9 +189,14 @@ def write_fasta(fasta_dict, out_path,flag='w'):
             wrappedseq = textwrap.wrap(seq, width=80)
             f.write('\n'.join(wrappedseq))
         f.write('\n')
+        
+def get_fastas_from_record(entrezrecordlist):
+    pass
 
+def write_fastas_to_file(somefastadict):
+    pass
 
-def get_fastas_from_entrez(handleobj, outputpath):
+def get_fastas_from_entrez(handleobj, outputpath, seqtype='protein'):
     """Takes an Entrez handle object 
     and saves the seqs in fasta files"""
     records = Entrez.parse(handleobj)
@@ -83,11 +204,10 @@ def get_fastas_from_entrez(handleobj, outputpath):
         outfile = f"{record['GBSeq_primary-accession']}.fasta"
         outpath = outputpath.joinpath(outfile)
         print(f"Writing to {outfile}")
-        fastadict = extract_fasta_info(record)
+        fastadict = extract_fasta_info(record, seqtype)
         write_fasta(fastadict, outpath)
         print('---> written')
 
-    
 def argumentsparser():
     parser = argparse.ArgumentParser(usage="python3 %(prog)s [-h] accidfile outpath",
                                      description="Retrieves seq from NCBI",)
@@ -96,13 +216,29 @@ def argumentsparser():
     args = parser.parse_args()
     return args
 
-if __name__=="__main__":
-    args = argumentsparser()
-    accidfilepath = Path(args.accidfile)
-    outpath = Path(args.outpath)
-    idlist = readaccids(accidfilepath)
-    testrecs = batch_get(idlist)
-    get_fastas_from_entrez(testrecs, outpath)
+# if __name__=="__main__":
+#     # args = argumentsparser()
+    # accidfilepath = Path(args.accidfile)
+    # outpath = Path(args.outpath)
+    # idlist = readaccids(accidfilepath)
+    # protrecs = batch_get_protein(idlist)
+    # genelist = get_gene_accid(protrecs)
+    # generecs = batch_get_nucleotide(genelist)
+    # get_fastas_from_entrez(generecs, outpath, 'nucleotide')
     
+    # accidfilepath=Path('accidsmall')
+    # idlist=readaccids(accidfilepath)
+    # protrecs = batch_get_protein(idlist)
+    # genelist = get_gene_accid(protrecs)
+    # generecs = batch_get_nucleotide(genelist)
 
         
+# protid = 'KAI4832452.1'
+
+# protrec = batch_get_protein([protid])
+
+# geneid = get_gene_accid(protrec)
+
+# generec = batch_get_nucleotide(geneid)
+
+# recs = Entrez.parse(generec)
